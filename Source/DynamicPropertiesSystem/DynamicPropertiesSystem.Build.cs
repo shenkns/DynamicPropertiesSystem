72e8da7addb382a8d31a8558db35e6eb5666debb
shenkns <shenkns@gmail.com>
2023-11-14 21:04:34 +0300

    [GR-152] Update for enhanced logging

--- Source/DynamicPropertiesSystem/DynamicPropertiesSystem.Build.cs
+++ Source/DynamicPropertiesSystem/DynamicPropertiesSystem.Build.cs
@@ -7,6 +7,7 @@ public class DynamicPropertiesSystem : ModuleRules
 	public DynamicPropertiesSystem(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+		CppStandard = CppStandardVersion.Cpp17;
 		
 		PublicIncludePaths.AddRange(
 			new string[] {
